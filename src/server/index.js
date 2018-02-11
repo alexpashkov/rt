bbcbf9d5566ccba48c50d373a715b7855d2bd1f1
Alex Pashkov <alexpashkov123@gmail.com>
2018-02-11 14:19:41 +0200

    configured seving of a public folder

--- src/server/index.js
+++ src/server/index.js
@@ -1,6 +1,7 @@
 'use strict';
 
-const app = require('express')();
+const express = require('express');
+const app = express();
 const http = require('http').Server(app);
 const path = require('path');
 const io = require('socket.io')(http);
@@ -8,7 +9,7 @@ const logger = require('./logger');
 
 require('dotenv').config({path: path.resolve(__dirname, '.env')});
 
-io.on('connection', () => logger.info('New connection'));
-
 const PORT = process.env.PORT || 3000;
+
+app.use(express.static(path.resolve(__dirname, 'public')));
 app.listen(PORT, () => logger.info(`Server is listening on port ${PORT}`));
