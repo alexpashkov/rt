b602ca375959cbddcc53fc2650d8be0be7d37551
Alex Pashkov <alexpashkov123@gmail.com>
2018-02-11 14:09:51 +0200

    configured .evn

--- src/server/index.js
+++ src/server/index.js
@@ -2,12 +2,13 @@
 
 const app = require('express')();
 const http = require('http').Server(app);
+const path = require('path');
 const io = require('socket.io')(http);
 const logger = require('./logger');
 
-require('./config');
+require('dotenv').config({path: path.resolve(__dirname, '.env')});
 
 io.on('connection', () => logger.info('New connection'));
 
 const PORT = process.env.PORT || 3000;
-app.listen(PORT, () => logger.info(`Server listening on port ${PORT}`));
+app.listen(PORT, () => logger.info(`Server is listening on port ${PORT}`));
