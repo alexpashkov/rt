0b16a2e4b821e8d10ce8e2388e6e6664dedb3231
Alex Pashkov <alexpashkov123@gmail.com>
2018-02-11 13:48:58 +0200

    configured logger

--- src/server/index.js
+++ src/server/index.js
@@ -1,12 +1,13 @@
-require("dotenv").config();
+'use strict';
 
-const app = require("express")();
-const http = require("http").Server(app);
-const io = require("socket.io")(http);
+const app = require('express')();
+const http = require('http').Server(app);
+const io = require('socket.io')(http);
+const logger = require('./logger');
+
+require('./config');
+
+io.on('connection', () => logger.info('New connection'));
 
 const PORT = process.env.PORT || 3000;
-
-const app = express();
-const server = http.createServer(app);
-
-server.listen(PORT, () => console.log(`Server listening on port ${PORT}`));
+app.listen(PORT, () => logger.info(`Server listening on port ${PORT}`));
---
+++ src/server/logger.js
@@ -0,0 +1,17 @@
+'use strict';
+
+const winston = require('winston');
+
+const tsFormat = () => new Date().toLocaleTimeString();
+const logger = new winston.Logger({
+  transports: [
+    // colorize the output to the console
+    new winston.transports.Console({
+      timestamp: tsFormat,
+      colorize: true,
+    }),
+  ],
+});
+logger.level = 'debug';
+
+module.exports = logger;
