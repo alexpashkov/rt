9a287875ae4c639eda196674ec47d38f5023f85a
Eric Lyahovets <elyahove@e1r6p12.unit.ua>
2018-03-25 20:21:33 +0300

    Rework handler assignment for events to reduce code density.

--- src/server/events/index.js
+++ src/server/events/index.js
@@ -5,6 +5,68 @@ const events = require("./types.js");
 const PlayersService = require("../services/PlayersService.js");
 const GamesService = require("../services/GamesService.js");
 
+function paramDecorator(handler, param) {
+  return (data, callback) => {
+    handler(param, data, callback);
+  };
+}
+
+function onGameCreate(params, callback) {
+  const player = params.player;
+
+  if (player.inGame()) {
+    callback({
+      "status":"error",
+      "description": "You are already in game."
+    });
+    return ;
+  }
+
+  player.gameId = GamesService.createGame(player.id);
+  player.socket.join(`game_${player.gameId}`);
+  callback({
+    "status": "success",
+    "gameId": player.gameId
+  });
+}
+
+function onGameJoin(params, callback) {
+  const player = params.player;
+
+  if (player.inGame()) {
+    callback({
+      "status": "error",
+      "description", "You are already in game."
+    });
+  }
+
+  const gameToJoin = GamesService.getGame(data.id);
+
+  if (gameToJoin) {
+    gameToJoin.addPlayer(player.id);
+    callback({ "status": "success" });
+  } else {
+    callback({
+      "status": "error",
+      "description": "Game with such ID does not exist"
+    });
+  }
+}
+
+function onDisconnect(params) {
+  const player = params.player;
+
+  logger.debug("Socket disconnected");
+
+  player.socket = null;
+
+  if (player.gameId) {
+    GamesService.getGame(player.gameId).notifyDisconnected(player.id);
+  }
+
+  player.free();
+}
+
 module.exports = server => {
   global.io = require("socket.io")(server);
 
@@ -22,55 +84,15 @@ module.exports = server => {
       return PlayersService.createPlayer(socket);
     })();
 
-    socket.emit("id", { id: player.id });
+    GamesService.subscribeSocketOnUpdates(socket);
 
-    socket.on(events.GAME_CREATE, (callback) => {
-      if (player.inGame()) {
-        callback({
-          "status": "error",
-          "description": "You are already in game."
-        });
-        return ;
-      }
-
-      player.gameId = GamesService.createGame(player.id);
-      socket.join(`game_${player.gameId}`);
-      callback({
-        "status": "success",
-        "gameId": player.gameId
-      });
+    socket.emit(events.PLAYER_CONNECTED, {
+      id: player.id,
+      gameId: player.inGame() ? player.getGameId() : null
     });
 
-    socket.on(events.GAME_JOIN, (data, callback) => {
-      if (player.inGame()) {
-        callback({
-          "status": "error",
-          "description": "You are already in game."
-        });
-        return;
-      }
-
-      const gameToJoin = GamesService.getGame(data.id);
-
-      if (gameToJoin) {
-        gameToJoin.addPlayer(player.id);
-        callback({
-          "status": "success"
-        });
-      } else {
-        callback({
-          "status": "error",
-          "description": "Game with such ID does not exist"
-        });
-      }
-    });
-
-    socket.on("disconnect", () => {
-      logger.debug("Socket disconnected");
-      player.socket = null;
-      if (player.gameId) {
-        GamesService.getGame(player.gameId).notifyDisconnected(player.id);
-      }
-    });
+    socket.on(events.GAME_CREATE, paramDecorator(onGameCreate, {player: player}));
+    socket.on(events.GAME_JOIN, paramDecorator(onGameJoin, {player: player}));
+    socket.on("disconnect", paramDecorator(onDisconnect, {player: player}));
   });
 };
