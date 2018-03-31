c181a7699c44dbfe403c6dfb29b5960cdb134077
Eric Lyahovets <elyahove@e1r6p10.unit.ua>
2018-03-31 18:39:07 +0300

    Delete unused events.

--- src/shared/types.js
+++ src/shared/types.js
@@ -1,15 +1,5 @@
 "use strict";
 
-
-exports.PLAYER_CONNECTED = "PLAYER_CONNECTED";
-exports.PLAYER_SET_LOGIN = "PLAYER_SET_LOGIN";
-
-exports.GAMES_UPDATE  = "GAMES_UPDATE";
-exports.GAMES_LIST    = "GAMES_LIST";
-exports.GAME_CREATE   = "GAME_CREATE";
-exports.GAME_JOIN     = "GAME_JOIN";
-exports.GAME_CHAT_MESSAGE_SEND = "GAME_CHAT_MESSAGE_SEND";
-
 module.exports = {
   client: {
     GAME_CREATE: "GAME_CREATE",
