7a4a59cf4a2d9b00abc686fc97ad33e4608316bd
Eric Lyahovets <elyahove@e1r6p10.unit.ua>
2018-03-31 20:17:14 +0300

    Create Games and Player controllers.

---
+++ src/server/controllers/GamesController.js
@@ -0,0 +1,6 @@
+
+
+class GamesController {
+}
+
+module.exports = new GamesController();
---
+++ src/server/controllers/PlayerController.js
@@ -0,0 +1,42 @@
+"use strict";
+
+const events = require("../../shared/types.js");
+const uniqid = require("uniqid");
+const GamesController = require("./GamesController");
+
+class PlayerController {
+  static manageConnection(socket) {
+    socket.playerController = new PlayerController(socket);
+  }
+
+  constructor(socket) {
+    this.socket = socket;
+    this.id = uniqid();
+    socket.on(events.client.GAME_CREATE, this.onGameCreate);
+    socket.on(events.client.GAME_JOIN, this.onGameJoin);
+    socket.on("disconnect", this.onDisconnect);
+  }
+
+  onGameCreate(callback) {
+    callback(this._respondSuccess({"gameId": 1}));
+  }
+
+  onGameJoin(data, callback) {
+    callback(this._respondSuccess());
+  }
+
+  onDisconnect() {
+    
+  }
+
+  _respondSuccess(data) {
+    return Object.assign({"status": "success"}, data);
+  }
+
+  _respondError(data) {
+    return Object.assign({"status": "error"}, data);
+  }
+
+}
+
+module.exports = PlayerController;
