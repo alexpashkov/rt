b4ca6368b20818f716c7bd6e0a5ed9e67148822d
Eric Lyahovets <elyahove@e1r6p10.unit.ua>
2018-04-01 17:35:20 +0300

    Remove unused code.

--- src/server/app.js
+++ src/server/app.js
@@ -1,9 +1,7 @@
 const express = require("express");
 const app = express();
 const path = require("path");
-const morgan = require("morgan")("tiny");
 
-// app.use(morgan);
 app.use(express.static(path.resolve(__dirname, "public")));
 app.all("*", (_, res) => res.sendStatus(404));
 
