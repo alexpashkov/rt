c1f968a7c2fe8b948e1d68f9d912b1ae9cb68b80
Alexander Pashkov <alexpashkov123@gmail.com>
2018-06-05 18:18:46 +0300

    add currentGameInfo propType

--- src/client/App/Game/Game.js
+++ src/client/App/Game/Game.js
@@ -14,9 +14,25 @@ export const Spinner = () => (
     <PacmanLoader color="red" />
   </div>
 );
-const Game = ({ gameId }) => <div>{gameId}</div>;
+const Game = ({ currentGameInfo, gameId }) => (
+  <div>
+    {gameId}
+    <pre>{JSON.stringify(currentGameInfo, null, 2)}</pre>
+  </div>
+);
 
 Game.propTypes = {
+  currentGameInfo: PropTypes.shape({
+    id: PropTypes.string.isRequired,
+    isRunning: PropTypes.bool.isRequired,
+    leaderId: PropTypes.string.isRequired,
+    players: PropTypes.arrayOf(
+      PropTypes.shape({
+        login: PropTypes.string.isRequired
+      })
+    ).isRequired,
+    chatHistory: PropTypes.array.isRequired
+  }).isRequired,
   gameId: PropTypes.string.isRequired
 };
 
