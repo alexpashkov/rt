53615323a4912b2091aaee1da245aa3aeeccce65
Alexander Pashkov <apashkov@student.unit.ua>
2018-06-09 17:46:51 +0300

    add base for board

--- src/client/App/Game/Game.js
+++ src/client/App/Game/Game.js
@@ -1,8 +1,9 @@
 /* eslint react/prop-types: 0 */ // prop-types are set in the container component
 import React from 'react';
+import GameBoard from './GameBoard/GameBoard';
 
-const Game = ({ currentGameInfo }) => {
-  return <pre>{JSON.stringify(currentGameInfo)}</pre>;
-};
+const board = Array.from(new Array(20)).fill(Array.from(new Array(10)));
+
+const Game = ({ currentGameInfo }) => <GameBoard board={board} />;
 
 export default Game;
---
+++ src/client/App/Game/GameBoard/GameBoard.js
@@ -0,0 +1,30 @@
+import React from 'react';
+import styled from 'react-emotion';
+
+const Cell = styled.div`
+  border: 1px solid red;
+  width: 10%;
+  padding-top: 10%;
+  background-color: ${({ filled, theme }) =>
+    filled ? theme.colors.accent : '#eee'};
+`;
+
+const Row = styled.div`
+  display: flex;
+  height: 5%;
+`;
+const BoardWrapper = styled.div`
+  width: 100%;
+  align-self: flex-start;
+  max-width: 400px;
+`;
+
+const GameBoard = ({ board }) => (
+  <BoardWrapper>
+    {board.map((row, y) => (
+      <Row key={y}>{row.map((cell, x) => <Cell key={x} filled={cell} />)}</Row>
+    ))}
+  </BoardWrapper>
+);
+
+export default GameBoard;
