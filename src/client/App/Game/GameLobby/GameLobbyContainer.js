b6eac80ac186b48357373e55dd2c0000a256b725
Alexander Pashkov <apashkov@student.unit.ua>
2018-06-09 18:38:46 +0300

    add GAME_STARTED event handling

--- src/client/App/Game/GameLobby/GameLobbyContainer.js
+++ src/client/App/Game/GameLobby/GameLobbyContainer.js
@@ -1,7 +1,10 @@
 import { connect } from 'react-redux';
-import { compose, withHandlers } from 'recompose';
+import { compose, withHandlers, lifecycle } from 'recompose';
 
-import { client as clientSocketEvents } from '../../../../shared/socket-events';
+import {
+  client as clientSocketEvents,
+  server as serverSocketEvents
+} from '../../../../shared/socket-events';
 import { setIsRunning } from '../../../actions/currentGameInfoActions';
 import socket from '../../../socket';
 
@@ -12,14 +15,20 @@ export default compose(
     setIsRunning
   }),
   withHandlers({
-    handleGameStart: ({ setIsRunning }) => () => {
-      socket.emit(clientSocketEvents.GAME_START, res => {
-        if (res.status === 'success') {
-          setIsRunning(true);
-          return;
-        }
-        console.warn(res);
-      });
+    handleGameStart: () => () => {
+      socket.emit(clientSocketEvents.GAME_START, res =>
+        console.log('game start ', res)
+      );
+    }
+  }),
+  lifecycle({
+    componentDidMount() {
+      const { setIsRunning } = this.props;
+      socket.on(serverSocketEvents.GAME_STARTED, () => setIsRunning(true));
+    },
+    componentWillUnmount() {
+      const { setIsRunning } = this.props;
+      socket.off(serverSocketEvents.GAME_STARTED);
     }
   })
 )(GameLobby);
