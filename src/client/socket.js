abb6b5431c8ee2122b272ed3eab70829913d6e9b
Alexander Pashkov <apashkov@student.unit.ua>
2018-06-10 18:10:03 +0300

    global variables are awesome

--- src/client/socket.js
+++ src/client/socket.js
@@ -6,4 +6,6 @@ import { setData } from './actions/userActions';
 const socket = io(process.env.REACT_APP_SERVER_URL);
 socket.on(server.USER_CONNECTED, userData => store.dispatch(setData(userData)));
 
+window.socket = socket; /* global variables are awesome (this is actually for debugging) */
+
 export default socket;
