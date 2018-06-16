9a648563ffc332d281c580622c040d91f42c072a
Alexander Pashkov <apashkov@student.unit.ua>
2018-06-16 19:43:29 +0300

    move game event listeners to GameContainer

--- src/client/App/Game/withRunningGameLogic.js
+++ src/client/App/Game/withRunningGameLogic.js
@@ -4,13 +4,7 @@ import socketEvents from '../../../shared/socket-events';
 
 export default compose(
   lifecycle({
-    componentDidMount() {
-      socket.on(socketEvents.server.GAME_PIECE_CURRENT, console.log);
-      socket.on(socketEvents.server.GAME_BOARD_CURRENT, console.log);
-    },
-    componentWillUnmount() {
-      socket.off(socketEvents.server.GAME_PIECE_CURRENT);
-      socket.off(socketEvents.server.GAME_BOARD_CURRENT);
-    }
+    componentDidMount() {},
+    componentWillUnmount() {}
   })
 );
