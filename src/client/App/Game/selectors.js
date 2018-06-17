fa38e9fcdd3a5cd3167e805371500723d1dbeb7a
Alexander Pashkov <apashkov@student.unit.ua>
2018-06-17 14:04:10 +0300

    add get piece cells

---
+++ src/client/App/Game/selectors.js
@@ -0,0 +1,35 @@
+import { numToBinary16String } from '../../utils';
+
+export const mergeBoardAndPiece = (board, piece) => {
+  if (!piece) return board;
+
+  const pieceCells = getPieceCells(piece.code);
+  return board.map(
+    (row, y) =>
+      pieceCells.find(cell => cell.y + piece.y === y)
+        ? row.map(
+            (cell, x) =>
+              !!pieceCells.find(
+                cell => cell.x + piece.x === x && cell.y + piece.y === y
+              )
+          )
+        : row
+  );
+};
+
+export const getPieceCells = pieceCode =>
+  numToBinary16String(pieceCode)
+    .split('')
+    .reduce(
+      (acc, curr, i) =>
+        Number(curr)
+          ? [
+              ...acc,
+              {
+                x: i % 4,
+                y: Math.floor(i / 4)
+              }
+            ]
+          : acc,
+      []
+    );
