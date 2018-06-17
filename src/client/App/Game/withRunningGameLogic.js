c34de65dd25cc2bacd5086049f0443d2020869d9
Alexander Pashkov <apashkov@student.unit.ua>
2018-06-17 18:40:11 +0300

    Added rotate emit.

--- src/client/App/Game/withRunningGameLogic.js
+++ src/client/App/Game/withRunningGameLogic.js
@@ -3,25 +3,28 @@ import { compose, lifecycle, withHandlers } from 'recompose';
 import socket from '../../socket';
 import socketEvents from '../../../shared/socket-events';
 
+const keyCodeToDirectionStringMap = {
+  37: 'left',
+  39: 'right',
+  40: 'down'
+};
+
 /* A bit of fun with functional programming, don't do it when other people ought to read this code */
-
-const keyCodeToDirection = new Map([[37, 'left'], [39, 'right'], [40, 'down']]);
-
 const handlePieceMovement = R.pipe(
   R.prop('keyCode'),
-  R.when(R.anyPass([R.equals(37), R.equals(39), R.equals(40)]), keyCode => {
+  R.when(R.anyPass([R.equals(37), R.equals(39), R.equals(40)]), keyCode =>
     socket.emit(
       socketEvents.client.GAME_PIECE_MOVE,
-      keyCodeToDirection.get(keyCode)
-    );
-  })
+      keyCodeToDirectionStringMap[keyCode]
+    )
+  )
 );
 
 const handlePieceRotation = R.pipe(
   R.prop('keyCode'),
-  R.when(R.equals(38), keyCode => {
-    console.error(keyCode);
-  })
+  R.when(R.equals(38), keyCode =>
+    socket.emit(socketEvents.client.GAME_PIECE_ROTATE, 'right')
+  )
 );
 
 export default compose(
