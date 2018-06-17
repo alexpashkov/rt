207620f01b94af899cb243c4dbc5936f5b832d59
Alexander Pashkov <apashkov@student.unit.ua>
2018-06-17 18:53:10 +0300

    add loggin of rotate attempt

--- src/server/models/Player.js
+++ src/server/models/Player.js
@@ -27,27 +27,23 @@ class Player {
     this.currentPiece = {
       code: this.handlers.getNewPiece(this.pieceIndex),
       x: 0,
-      y: 0,
+      y: 0
     };
     this.incrementPieceIndex();
     this.onCurrentPieceUpdate();
   }
 
   movePiece(movementDirection) {
-    if (!this.currentPiece)
-      return false;
+    if (!this.currentPiece) return false;
 
     /*
      *  XXX: Separate as helper.
      */
     if (typeof movementDirection === 'string') {
       movementDirection = (() => {
-        if (movementDirection === 'down')
-          return {x: 0, y: 1};
-        else if (movementDirection === 'left')
-          return {x: -1, y: 0};
-        else if (movementDirection === 'right')
-          return {x: 1, y: 0};
+        if (movementDirection === 'down') return { x: 0, y: 1 };
+        else if (movementDirection === 'left') return { x: -1, y: 0 };
+        else if (movementDirection === 'right') return { x: 1, y: 0 };
       })();
     }
 
