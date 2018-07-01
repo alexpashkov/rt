ebf35e1bd437f44b03fb59ae86787bb906556431
Alexander Pashkov <alexpashkov123@gmail.com>
2018-07-01 18:17:05 +0300

    add testURL

--- src/client/App/App.test.js
+++ src/client/App/App.test.js
@@ -1,8 +1,7 @@
 import React from 'react';
-import renderer from 'react-test-renderer';
+import ReactDOM from 'react-dom';
 import App from './App';
 
-test("doesn't throw", () => {
-  const component = renderer.create(<App />);
-  expect(component.toJSON()).toMatchSnapshot();
+test('mounts', () => {
+  ReactDOM.render(<App />, document.createElement('div'));
 });
