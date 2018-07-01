fb654c6ab104e38eefe4370b62d70841babd0f68
Alexander Pashkov <alexpashkov123@gmail.com>
2018-07-01 17:51:04 +0300

    corrected typos

--- src/client/App/App.test.js
+++ src/client/App/App.test.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import renderer from 'react-test-renderer';
+import App from './App';
 
 test("doesn't throw", () => {
   const component = renderer.create(<App />);
--- src/server/models/Room.js
+++ src/server/models/Room.js
@@ -1,13 +1,13 @@
-"use strict";
+'use strict';
 
-const UserService = require("../services/UserService.js");
-const { EventEmitter } = require("events");
+const UserService = require('../services/UserService.js');
+const { EventEmitter } = require('events');
 
 const RoomEvents = {
-  R_STARTED: "R_STARTED",
-  R_START_FAILED: "R_START_FAILED",
-  R_INFO_UPDATE: "R_INFO_UPDATE",
-  R_CHAT_MESSAGE: "R_CHAT_MESSAGE",
+  R_STARTED: 'R_STARTED',
+  R_START_FAILED: 'R_START_FAILED',
+  R_INFO_UPDATE: 'R_INFO_UPDATE',
+  R_CHAT_MESSAGE: 'R_CHAT_MESSAGE'
 };
 
 class Room extends EventEmitter {
@@ -21,23 +21,23 @@ class Room extends EventEmitter {
   }
 
   playerJoin(userController) {
-    if (this.players.includes(userController))
-      return false;
+    if (this.players.includes(userController)) return false;
 
-    if (this.destroyTimeout)
-      this.cancelDestroyTimeout();
+    if (this.destroyTimeout) this.cancelDestroyTimeout();
 
     this.setEventHandlersForPlayer(userController);
     this.roomInfoUpdated();
     return true;
   }
 
-  playerLeave() {
-
-  }
+  playerLeave() {}
 
   chatMessageSend(message) {
-    if (message.message && message.login && this.isPlayerInRoomByLogin(message.login)) {
+    if (
+      message.message &&
+      message.login &&
+      this.isPlayerInRoomByLogin(message.login)
+    ) {
       this.chatHistory.push(message);
       this.emit(RoomEvents.R_CHAT_MESSAGE, message);
     }
@@ -46,23 +46,21 @@ class Room extends EventEmitter {
   getRoomInfo() {
     return {
       id: this.id,
-      leaderId: (this.players[0] && this.players[0].id),
-      players: this.players.map((playerId) => {
-        const user = UserService.getUserById(playerId)
+      leaderId: this.players[0] && this.players[0].id,
+      players: this.players.map(playerId => {
+        const user = UserService.getUserById(playerId);
         return user ? { login: user.getLogin() } : undefined;
       }),
-      chatHistory: this.chatHistory;
+      chatHistory: this.chatHistory
     };
   }
 
   isPlayerInRoomByLogin(login) {
     return (
-      this.players
-      .filter(playerId => {
+      this.players.filter(playerId => {
         const user = UserService.getUserById(playerId);
         return user ? user.getLogin() === login : false;
-      })
-      .length !== 0;
+      }).length !== 0
     );
   }
 
