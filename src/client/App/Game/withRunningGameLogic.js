928d1a423812e78a6370c0d5162c1285455387be
Alexander Pashkov <apashkov@student.unit.ua>
2018-07-21 16:39:12 +0300

    Add keydown key slip with throttle

--- src/client/App/Game/withRunningGameLogic.js
+++ src/client/App/Game/withRunningGameLogic.js
@@ -2,6 +2,7 @@ import * as R from 'ramda';
 import { compose, lifecycle, withHandlers } from 'recompose';
 import socket from '../../socket';
 import socketEvents from '../../../shared/socket-events';
+import throttle from 'lodash.throttle';
 
 const keyCodeToDirectionStringMap = {
   37: 'left',
@@ -10,35 +11,38 @@ const keyCodeToDirectionStringMap = {
 };
 
 /* A bit of fun with functional programming, don't do it when other people ought to read this code */
-const handlePieceMovement = R.pipe(
-  R.prop('keyCode'),
-  R.when(R.anyPass([R.equals(37), R.equals(39), R.equals(40)]), keyCode =>
-    socket.emit(
-      socketEvents.client.GAME_PIECE_MOVE,
-      keyCodeToDirectionStringMap[keyCode]
+const handlePieceMovement = throttle(
+  R.pipe(
+    R.prop('keyCode'),
+    R.when(R.anyPass([R.equals(37), R.equals(39), R.equals(40)]), keyCode =>
+      socket.emit(
+        socketEvents.client.GAME_PIECE_MOVE,
+        keyCodeToDirectionStringMap[keyCode]
+      )
     )
-  )
+  ),
+  75
 );
 
-const handlePieceRotation = R.pipe(
-  R.prop('keyCode'),
-  R.when(
-    R.equals(38),
-    keyCode =>
-      console.log('rotate') ||
+const handlePieceRotation = throttle(
+  R.pipe(
+    R.prop('keyCode'),
+    R.when(R.equals(38), () =>
       socket.emit(socketEvents.client.GAME_PIECE_ROTATE, 'right')
-  )
+    )
+  ),
+  75
 );
 
 export default compose(
   lifecycle({
     componentDidMount() {
-      document.addEventListener('keyup', handlePieceMovement);
-      document.addEventListener('keyup', handlePieceRotation);
+      document.addEventListener('keydown', handlePieceMovement);
+      document.addEventListener('keydown', handlePieceRotation);
     },
     componentWillUnmount() {
-      document.removeEventListener('keyup', handlePieceMovement);
-      document.removeEventListener('keyup', handlePieceRotation);
+      document.removeEventListener('keydown', handlePieceMovement);
+      document.removeEventListener('keydown', handlePieceRotation);
     }
   })
 );
