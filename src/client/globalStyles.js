afa49a2eb2f3d3c5dc75fc700582eb2b6326674c
Alexander Pashkov <apashkov@student.unit.ua>
2018-07-22 18:31:26 +0300

    change font

--- src/client/globalStyles.js
+++ src/client/globalStyles.js
@@ -2,22 +2,19 @@ import { injectGlobal } from 'react-emotion';
 import 'normalize.css/normalize.css';
 
 injectGlobal`
-    @import url('https://fonts.googleapis.com/css?family=Comfortaa:700|Passion+One');
+    @import url('https://fonts.googleapis.com/css?family=Changa+One');
     *, *::after, *::before {
       box-sizing: border-box;
     }
     html {
-      font-size: 62.5%;  
+      font-family: 'Changa One', cursive;
+      font-size: 62.5%;
     }
     body {
       margin: 0; 
-      font-size: 1.5rem;
-      line-height: 1.5;
-      font-family: 'Comfortaa', cursive;
-      font-weight: 700;
+      font-size: 1.6rem;
+      * {
+        letter-spacing: 0.03em !important;
+      }
     } 
-    h1, h2, h3, h4, h5, h6 {
-      font-family: 'Passion One', cursive;
-      letter-spacing: 0.05em;
-    }
 `;
